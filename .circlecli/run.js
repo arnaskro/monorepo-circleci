725f474608de9b9590675b05ab10db92acc18bd2
Arnas Kromelis <arnas.kromelis@gmail.com>
2018-12-22 13:26:45 +0400

    initial circleci

---
+++ .circlecli/run.js
@@ -0,0 +1,32 @@
+const { readFileSync } = require("fs");
+const { exec } = require("child_process");
+const { CIRCLE_API_USER_TOKEN = "" } = process.env;
+const GIT_COMMAND =
+  'git diff --no-commit-id --name-only -r `git log -n 2 --oneline --pretty=format:"%h" | tail -n1` | cut -d/ -f1 | sort -u >  modifiedProjects';
+
+const readFileContent = fileName => {
+  try {
+    return readFileSync(fileName, "utf-8")
+      .split("\n")
+      .filter(Boolean);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const launchADirectoryJob = directory => {
+  exec(`curl -s -u ${CIRCLE_API_USER_TOKEN} \
+                -d build_parameters[CIRCLE_JOB]=${directory} \
+                https://circleci.com/api/v1.1/project/github/devtoni/sample-monorepo-circleci/tree/master`);
+};
+
+const isInDirectoryRoot = directoriesOnRoot => directory =>
+  directoriesOnRoot.includes(directory);
+
+;(() => {
+  exec(GIT_COMMAND);
+  const modifiedDirectories = readFileContent("modifiedProjects");
+  const directoriesOnRoot = readFileContent("/.circleci/projects.txt");
+  const directoriesToTriggerAJob = modifiedDirectories.filter(isInDirectoryRoot(directoriesOnRoot));
+  directoriesToTriggerAJob.forEach(launchADirectoryJob);
+})();
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Circle CI monorepo Demo 
 
 This repository is an experiment about building monorepo projects using [Circle CI](https://circleci.com/).
-Be sure to checkout my [Travis CI monorepo demo](https://github.com/arnaskro/monorepo-travis) if you are interested.
+
+Be sure to checkout my [Travis CI monorepo demo](https://github.com/arnaskro/monorepo-travis) if you are interested in other CI options.
