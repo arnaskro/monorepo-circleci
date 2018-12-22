8167dd042902208004bb5c32686067d17b6252bc
Arnas Kromelis <arnas.kromelis@gmail.com>
2018-12-22 13:32:58 +0400

    changes

--- projects/projectB/__tests__/index-test.js
+++ projects/projectB/__tests__/index-test.js
@@ -4,6 +4,10 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
+test('adds 14 + 6 to equal 20', () => {
+  expect(sum(14, 6)).toBe(20);
+});
+
 test('adds 30 + -120 to equal -90', () => {
   expect(sum(30, -120)).toBe(-90);
 });
