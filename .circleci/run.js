4d4f2d6a54af9c7c634020a4a9df4a2777772296
Arnas Kromelis <arnas.kromelis@gmail.com>
2018-12-22 14:31:26 +0400

    changes

--- .circleci/run.js
+++ .circleci/run.js
@@ -1,7 +1,7 @@
 const { readFileSync } = require("fs");
 const { exec } = require("child_process");
-const { CIRCLE_API_USER_TOKEN = "", CIRCLE_SHA1 } = process.env;
-const GIT_COMMAND = () => `git diff --no-commit-id --name-only | sort -u | uniq | grep $2 > modifiedProjects`;
+const { CIRCLE_API_TOKEN = "", CIRCLE_PROJECT_USERNAME, CIRCLE_PROJECT_REPONAME, CIRCLE_BRANCH } = process.env;
+const GIT_COMMAND = (project) => `git diff --no-commit-id --name-only | sort -u | uniq | grep ${project} > isModified`;
 
 const readFileContent = fileName => {
   try {
@@ -13,27 +13,30 @@ const readFileContent = fileName => {
   }
 };
 
+const isModified = (project) => exec(GIT_COMMAND(project)) && readFileContent("isModified");
+
 const launchADirectoryJob = directory => {
-  console.log(directory)
-  console.log(CIRCLE_API_USER_TOKEN)
-  exec(`curl -s -u ${CIRCLE_API_USER_TOKEN} \
+  exec(`curl -s -u ${CIRCLE_API_TOKEN} \
                 -d build_parameters[CIRCLE_JOB]=${directory} \
-                https://circleci.com/api/v1.1/project/github/devtoni/sample-monorepo-circleci/tree/master`);
+                https://circleci.com/api/v1.1/project/github/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/tree/${CIRCLE_BRANCH}`);
 };
 
-const isInDirectoryRoot = directoriesOnRoot => directory =>
-  directoriesOnRoot.includes(directory);
+const isInDirectoryRoot = allProjects => project => allProjects.includes(project);
 
 ;(() => {
-  console.log("CIRCLE_API_USER_TOKEN")
-  console.log(CIRCLE_API_USER_TOKEN)
-  exec(GIT_COMMAND);
-  const modifiedDirectories = readFileContent("modifiedProjects");
-  console.log('modifiedDirectories')
-  console.log(modifiedDirectories)
-  const directoriesOnRoot = readFileContent("./.circleci/projects.json");
-  console.log('directoriesOnRoot')
-  console.log(directoriesOnRoot)
+  console.log("CIRCLE_API_TOKEN")
+  console.log(CIRCLE_API_TOKEN)
+  
+  const allProjects = readFileContent("./.circleci/projects.txt");
+  const modifiedProjects = allProjects.filter(isModified);
+  // .forEach(isInDirectoryRoot(allProjects))
+  console.log(modifiedProjects)
+
+
+
+
+
+
   // const directoriesToTriggerAJob = modifiedDirectories.filter(isInDirectoryRoot(directoriesOnRoot));
   // console.log("directoriesToTriggerAJob")
   // console.log(directoriesToTriggerAJob)
